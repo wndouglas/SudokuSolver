b0078dbd6ac161bb4b1a6c7594dc1203470d7eab
Will Douglas <wndouglas@hotmail.co.ukgit config --global user.name Will>
2018-07-27 19:29:43 +0100

    Initial Commit

---
+++ SudokuSolver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SudokuSolver
+//
+//  Created by Will Douglas on 27/07/2018.
+//  Copyright © 2018 Will Douglas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
